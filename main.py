ec0d42fc82d27319d20a93f1895ebce8d5cd56d4
Matt London <mel174@gmail.com>
2020-12-05 17:47:40 -0700

    added celsius to fahrenheit conversion

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import RPi.GPIO as GPIO
 import dht11
 
-print(__name__)
+def convert_c_to_f(degreesC):
+	return degreesC * 1.8 + 32
+
 def main():
+	print("Starting main function") 
 	#init GPIO
 	GPIO.setwarnings(False)
 	GPIO.setmode(GPIO.BCM)
 	GPIO.cleanup()
 
 	#read data using pin14
-	#TODO: make pin number a cmd line arg
 	instance = dht11.DHT11(pin = 14)
 	result = instance.read()
 
 	if result.is_valid():
-		print("Temperature: %-3.1f C" % result.temperature)
+		print("Temperature (C): %-3.1f C" % result.temperature)
+		print("Temperature (F):	{}".format(convert_c_to_f(result.temperature)))
 		print("Humidity: %-3.1f %%" % result.humidity)
 	else:
 		print("Error: %d" % result.error_code)
 
-if __name__ == "__main_":
+if __name__ == "__main__":
 	main()
